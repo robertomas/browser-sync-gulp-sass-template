28f556e7f37c16cc484303a15a6b51dfea73ae43
Roberto Mas <rmas@aussisimple.com>
2019-07-10 14:33:38 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# browser-sync-gulp-sass-template
-browser-sync-gulp-sass-template
+## browser-sync-gulp-sass-template
+Simple HTML and SASS boilerplate template with hot reload
+
+## Run code
+Simply browse to folder and type
+
+```bash
+npm i && gulp watch
+```
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+var sass = require('gulp-sass');
+
+gulp.task('browser-sync', function() {
+  bs.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(bs.reload({stream: true})); // prompts a reload after compilation
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+  gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', bs.reload);
+});
